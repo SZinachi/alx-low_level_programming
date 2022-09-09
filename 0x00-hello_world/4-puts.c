df47e9101c4ba58321882a961faccd137fd6f327
SZianchi <okorozinachi@gmail.com>
2022-09-08 22:04:06 -0700

    new file added

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/**
+ *
+ *  * main - Entry point in program execution
+ *
+ *   * Return: gives an output of 0 (Success)
+ *
+ *    */
+
+
+
+int main(void)
+
+{
+
+	puts("\"Programming is like building a multilingual puzzle");
+
+	return (0);
+
+}
