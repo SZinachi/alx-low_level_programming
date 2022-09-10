27c67eea66dcb05484643f06c8c89edc496ce2c7
SZianchi <okorozinachi@gmail.com>
2022-09-09 22:16:21 -0700

    changes amde

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -17,21 +17,21 @@ int n;
 
 char ch;
 
- for (n = 48; n < 58; n++)
+for (n = 48; n < 58; n++)
 
 {
 										putchar(n);
 										}
 
- for (ch = 'a'; ch <= 'f'; ch++)
+for (ch = 'a'; ch <= 'f'; ch++)
 
 {
-										
-putchar(ch);
+
+  putchar(ch);
 
 }
 
-putchar('\n');
+  putchar('\n');
 
 return (0);
 
