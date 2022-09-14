bb2280e4412a91a228c387501244ece2e4c24654
SZianchi <okorozinachi@gmail.com>
2022-09-14 14:02:39 -0700

    new file added

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -17,24 +17,18 @@ if (n >= 98)
 
 while (n > 98)
 
-{
-
 printf("%d, ", n--);
 
 printf("%d\n", n);
+}
 
-}
-}
 else
 
 {
 
 while (n < 98)
-
-{
-
 printf("%d, ", n++);
 printf("%d\n", n);
 }
 }
-}
+
