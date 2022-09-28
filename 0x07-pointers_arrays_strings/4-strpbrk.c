d1701ecbea084aefafb3bc96d47bfe58eea60b76
SZianchi <okorozinachi@gmail.com>
2022-09-28 10:05:16 -0700

    new file added

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <stdio.h>
+
+
+/**
+ * _strpbrk - Entry point
+ *
+ * @s: input
+ *
+ * @accept: input
+ *
+ * Return: Always 0 (Success)
+ */
+
+char *_strpbrk(char *s, char *accept)
+{
+int i, j;
+for (i = 0; s[i] != '\0'; i++)
+{
+for (j =0; accept[j] != '\0'; j++)
+{
+if (accept[j] == s[i])
+{
+return (s + i);
+}
+}
+}
+return (NULL);
+}
