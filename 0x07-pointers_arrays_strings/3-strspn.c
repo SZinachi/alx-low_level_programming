da2941423f4f616c7b003314588418fca8182bd7
SZianchi <okorozinachi@gmail.com>
2022-09-28 07:27:40 -0700

    new file dded

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -17,9 +17,9 @@ value = 0;
 for (i = 0; s[i] != '\0'; i++)
 {
 check = 0;
-for (j = 0; accept[i] != '\0'; j++)
+for (j = 0; accept[j] != '\0'; j++)
 {
-if (accept[i] == s[i])
+if (accept[j] == s[i])
 {
 value++;
 check = 1;
