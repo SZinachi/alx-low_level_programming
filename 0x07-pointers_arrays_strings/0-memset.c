38a1f224c6b9048e40332876edaf433ab6a936ed
SZianchi <okorozinachi@gmail.com>
2022-09-27 23:26:02 -0700

    changes made in 0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,7 +11,6 @@
  * @n: bytes
  *
  * Return: Always 0 (Success)
- *
  */
 
 char *_memset(char *s, char b, unsigned int n)
@@ -19,5 +18,5 @@ char *_memset(char *s, char b, unsigned int n)
 unsigned int i;
 for(i = 0; i < n; i++)
 s[i] = b;
-return(s);
+return (s);
 }
