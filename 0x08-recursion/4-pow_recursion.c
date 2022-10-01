e34d111e97916583932f38ad8a29dba861d6a31c
SZianchi <okorozinachi@gmail.com>
2022-10-01 11:32:32 -0700

    changes made to 4-pow_recursion.c

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -4,7 +4,7 @@
  * _pow_recursion - return the value of x raised to power y
  *
  * @x: integer
- *
+i *
  * @y: number to which x will be powered to
  *
  * Return: x to power y
@@ -15,6 +15,8 @@ int _pow_recursion(int x, int y)
 if (y < 0)
 return (-1);
 else if (y == 0)
+return (1);
+else if (y == 1)
 return (x);
 else
 return (x * (_pow_recursion(x, y-1)));
